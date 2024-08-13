98247bf7f28d60a7740136462e3d2cc527a834f1
Abdulqadir Abdillahi <abdiqaadirabdillahi@gmail.com>
2024-08-13 13:35:51 +0100

    header component

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+      <header>
+        <div className="logo">
+          <h1>🔥Fireplace Palace</h1>
+        </div>
+      </header>
+    );
+  }
+  
+  export default Header;
